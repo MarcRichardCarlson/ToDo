357bad4cd5b43bebd4ce5fed05695055ef35948d
Marc Carlson <MarcRichardCarlson@users.noreply.github.com>
2023-02-03 17:32:23 +0100

    Last Push

--- script.js
+++ script.js
@@ -2,136 +2,130 @@ let items = [];
 
 //Get
 function getData() {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(res => res.json())
-  .then(content => {
-    items = content
-    _displayItems()
-  })
-  .catch(er => console.error(er));
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.json())
+        .then((content) => {
+            items = content;
+            _displayItems();
+        })
+        .catch((er) => console.error(er));
 }
 getData();
 
 //Post
 
 function addData() {
-  const addTaskTextbox = document.getElementById('task');
-  if(addTaskTextbox.value == '') {
-    alert('Failed: Task needed')
-    return;
-  }else{
-    //console.log(items.length)
-    const newItem = {
-      completed: false,
-      title: addTaskTextbox.value,
-      userId: 1
+    const addTaskTextbox = document.getElementById("task");
+    if (addTaskTextbox.value == "") {
+        alert("Failed: Task needed");
+        return;
+    } else {
+        //console.log(items.length)
+        const newItem = {
+            completed: false,
+            title: addTaskTextbox.value,
+            userId: 1,
+        };
+
+        fetch("https://jsonplaceholder.typicode.com/todos/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newItem),
+        })
+            .then((res) => res.json())
+            .then((content) => {
+                updateItemsList(content);
+                _displayItems();
+            })
+            .catch((er) => console.log(er));
     }
-
-    fetch('https://jsonplaceholder.typicode.com/todos/', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(newItem),
-  
-  })
-
-  .then((res) => res.json())
-  .then((content) => {
-    updateItemsList(content)
-    _displayItems()
-  })
-  .catch(er => console.log(er));
-  }
 }
 
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addData()
-  _displayCount(items.length +1)
-})
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addData();
+    _displayCount(items.length + 1);
+});
 
 function updateItemsList(content) {
-  const newContent = {
-    id: items.length +1,
-    completed: content.completed,
-    title: content.title,
-    userId: content.userId
-  }
+    const newContent = {
+        id: items.length + 1,
+        completed: content.completed,
+        title: content.title,
+        userId: content.userId,
+    };
 
-  items.unshift(newContent)
-  console.log(newContent)
+    items.unshift(newContent);
+    console.log(newContent);
 }
 
 function deleteData(id) {
-  fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-    method: 'DELETE'
-  })
-  .then(res => {
-    if (res.ok) {
-      console.log(`Task ${id} deleted successfully.`);
-    } else {
-      console.log(`Error deleting resource ${id}.`);
-    }
-  })
-  .catch(er => console.log(er));
-
-    items = items.filter(function(item) {
-      return item.id !== id
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: "DELETE",
     })
+        .then((res) => {
+            if (res.ok) {
+                console.log(`Task ${id} deleted successfully.`);
+            } else {
+                console.log(`Error deleting resource ${id}.`);
+            }
+        })
+        .catch((er) => console.log(er));
 
-    _displayItems()
-    _displayCount(items.length -1)
+    items = items.filter(function (item) {
+        return item.id !== id;
+    });
+
+    _displayItems();
+    _displayCount(items.length - 1);
 }
 
 //Task Counter
 function _displayCount(itemCount) {
-  const title = (itemCount === 1) ? 'to-do' : 'to-dos';
-  document.getElementById('counter').innerText = `${itemCount} ${title}`;
+    const title = itemCount === 1 ? "to-do" : "to-dos";
+    document.getElementById("counter").innerText = `${itemCount} ${title}`;
 }
-
 
 function _displayItems() {
+    const tBody = document.getElementById("todos");
+    tBody.innerHTML = "";
 
-  const tBody = document.getElementById('todos');
-  tBody.innerHTML = '';
+    _displayCount(items.length);
 
-  _displayCount(items.length);
+    const button = document.createElement("button");
 
-  const button = document.createElement('button');
+    items.forEach((item) => {
+        let completedCheckbox = document.createElement("input");
+        completedCheckbox.type = "checkbox";
+        completedCheckbox.checked = item.completed;
 
-  items.forEach(item => {
-    let completedCheckbox = document.createElement('input');
-    completedCheckbox.type = 'checkbox';
-    completedCheckbox.checked = item.completed;
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = "Delete";
+        deleteButton.classList.add("btn");
+        deleteButton.classList.add("btn-danger");
+        deleteButton.addEventListener("click", () => {
+            deleteData(item.id);
+        });
 
-    let deleteButton = button.cloneNode(false);
-    deleteButton.innerText = 'Delete';
-    deleteButton.classList.add('btn');
-    deleteButton.classList.add('btn-danger');
-    deleteButton.addEventListener('click', () => {
-      deleteData(item.id)
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        td1.className = "border border-3 border-dark";
+        td1.appendChild(completedCheckbox);
+
+        let td2 = tr.insertCell(1);
+        let textNode = document.createTextNode(item.title);
+        td2.className = "border border-3 border-dark";
+        td2.appendChild(textNode);
+
+        let td3 = tr.insertCell(2);
+        td3.className = "border border-3 border-dark";
+        td3.appendChild(deleteButton);
     });
-
-    let tr = tBody.insertRow();
-    
-    let td1 = tr.insertCell(0);
-    td1.className = "border border-3 border-dark";
-    td1.appendChild(completedCheckbox);
-
-    let td2 = tr.insertCell(1);
-    let textNode = document.createTextNode(item.title);
-    td2.className = "border border-3 border-dark";
-    td2.appendChild(textNode);
-
-    let td3 = tr.insertCell(2);
-    td3.className = "border border-3 border-dark";
-    td3.appendChild(deleteButton);
-  });
 }
-
 
 /*
 //Put
